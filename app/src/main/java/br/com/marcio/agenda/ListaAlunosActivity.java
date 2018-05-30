2a563d4ec569cfc8e5c8a3b00e7115a84bde43d2
Marcio Vinicius <marcio.campos@outlook.com>
2018-05-29 23:43:47 -0300

    Aula 03 - Adicionando Floating Action Button

--- app/src/main/java/br/com/marcio/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/marcio/agenda/ListaAlunosActivity.java
@@ -1,8 +1,12 @@
 package br.com.marcio.agenda;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
@@ -12,13 +16,24 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        String[] alunos = {"Daniel", "Ronaldo", "Jeferson", "Felipe"};
+        String[] alunos = {"Daniel","Ronaldo","Jeferson","Felipe","Ronaldo","Jeferson","Felipe","Ronaldo","Jeferson" , "Felipe"};
 
         ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
 
         listaAlunos.setAdapter(adapter);
+
+        FloatingActionButton novoAluno = (FloatingActionButton) findViewById (R.id.novo_aluno);
+        novoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v)  {
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
+
+
 
     }
 }
