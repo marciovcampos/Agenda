3458f53b713e726bf8c4f4b783542675cd6e506d
Marcio Vinicius <marcio.campos@outlook.com>
2018-05-30 00:16:46 -0300

    Aula 03 - Movendo o botão salvar para a ActionBar

--- app/src/main/java/br/com/marcio/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/marcio/agenda/FormularioActivity.java
@@ -2,6 +2,9 @@ package br.com.marcio.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,14 +16,27 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+    }
 
-        botaoSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(FormularioActivity.this, "Aluno salvo com sucesso!", Toast.LENGTH_SHORT).show();
-            }
-        });
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_formulario_salvar:
+                Toast.makeText(FormularioActivity.this, "Aluno salvo!", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
 
     }
 
