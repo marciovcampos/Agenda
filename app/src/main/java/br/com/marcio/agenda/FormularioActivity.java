343b7c3d1379d6493c537f2de68438acacd5ea10
Marcio Vinicius <marcio.campos@outlook.com>
2018-05-30 23:41:25 -0300

    Aula 04 - Recuperando dados do Aluno

--- app/src/main/java/br/com/marcio/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/marcio/agenda/FormularioActivity.java
@@ -7,14 +7,21 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import br.com.marcio.agenda.modelo.Aluno;
+
 public class FormularioActivity extends AppCompatActivity {
+
+    private FormularioHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        helper = new FormularioHelper(this);
 
     }
 
@@ -31,7 +38,12 @@ public class FormularioActivity extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.menu_formulario_salvar:
-                Toast.makeText(FormularioActivity.this, "Aluno salvo!", Toast.LENGTH_SHORT).show();
+
+                Aluno aluno = helper.getAluno();
+
+                Toast.makeText(FormularioActivity.this, "Aluno "+aluno.getNome()+" salvo!", Toast.LENGTH_SHORT).show();
+
+
                 finish();
                 break;
         }
