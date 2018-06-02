6e15ddb0856e2afae941ab52d217e9a48c33014f
Marcio Vinicius <marcio.campos@outlook.com>
2018-06-02 00:38:24 -0300

    Android II - Aula 01 - Trabalhando com mapas e SMS

--- app/src/main/java/br/com/marcio/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/marcio/agenda/ListaAlunosActivity.java
@@ -80,6 +80,16 @@ public class ListaAlunosActivity extends AppCompatActivity {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
         final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
 
+        MenuItem itemSMS = menu.add("Enviar SMS");
+        Intent intentSMS = new Intent(Intent.ACTION_VIEW);
+        intentSMS.setData(Uri.parse("sms: "+aluno.getTelefone()));
+        itemSMS.setIntent(intentSMS);
+
+        MenuItem itemMapa = menu.add("Visualizar no mapa");
+        Intent intentMapa = new Intent(Intent.ACTION_VIEW);
+        intentMapa.setData(Uri.parse("geo:0,0?q= "+aluno.getEndereco()));
+        itemMapa.setIntent(intentMapa);
+
         MenuItem itemSite = menu.add("Visitar site");
         Intent intentSite = new Intent(Intent.ACTION_VIEW);
 
